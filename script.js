24a913a4f0e33a723a0453ebb677cf33c7393a40
Shihan Rishad <165176498+ShihanRishad@users.noreply.github.com>
2024-08-03 11:49:54 +0600

    Fixed a bug

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function loadWordList() {
-        fetch('https://shihanrishad.github.io/wordle-game/wordlist.txt')
+        fetch('wordlist.txt')
             .then(response => response.text())
             .then(data => {
                 wordList = data.split('\n').map(word => word.trim().toUpperCase());
